f525f6002d2e60199433b4ba67ff785d536bedba
Leah Cardoz <leahcardoz@gmail.com>
2022-11-01 10:51:36 -0400

    commit tree f0bce357f8489ed1de14466cef71ca32ddd3fea3

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,10 @@
-// Code Keypad Component Here
-
 function Keypad (){
+    const handleChange = () => console.log("Entering password...")
+
     return (
-        <div></div>
+        <div>
+            <input type="password" onChange={ handleChange }></input>
+        </div>
     )
 }
 
